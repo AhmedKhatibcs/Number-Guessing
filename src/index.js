64b6138c6f84dbe8441118503a4ef8ec9229927f
AhmedKhatibcs <ahmedkhatibcs@gmail.com>
2026-09-07 14:01:08 -0700

    Play Again works now

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-function randomNumber(){
-    return Math.floor(Math.random()*100);
-    console.log(randomNumber());
-}
-
-
 //global variable
 let tries = 10;
 GameStart();
+
+function randomNumber(){
+    return Math.floor(Math.random()*100);
+}
+
+
 function GameStart(){
     if(tries == 0){
         tries = 10;
@@ -21,28 +21,32 @@ function GameStart(){
 function triesLeft(userAnswer){
       if (tries == 1){
             console.log("sorry out of luck!");
+            tries -= 1;
             document.getElementById("tries").innerText = "sorry out of luck! Play Again, type \"yes";
             document.querySelector("#guessInput").value = "";
+            console.log(tries);
             //grabs "empty string"
-            tries -= 1;
     }
 
-         if (tries > 0){
+          else if(tries > 0) {
             tries -= 1;
             document.getElementById("tries").innerText = ("Tries "+ tries);
-            console.log(userAnswer);
             console.log(userAnswer);
             console.log(tries); 
     }
 }
 
+
+function validChecker(input){
+   
+}
 //Function: Invalid Checker
 function invalidChecker(userAnswer){
     //return true for invalid so string (works)|| constraints (dosen't)
     if (isNaN(userAnswer) || (0 > userAnswer  || userAnswer > 99)){
         console.log("Sorry Invalid");
         document.querySelector("#guessInput").placeholder = "Invalid";
-        console.log(userAnswer);
+        // console.log(userAnswer);
         // Checking the box now in that moment
         newAnswer = document.querySelector("#guessInput").value = "";
 }
@@ -50,23 +54,35 @@ function invalidChecker(userAnswer){
 
 //Function: 
 
+function WaitingGame(){
+    if(tries == 0){
+            return true;
+    }
+    else{
+        return false
+    }
+}
+
+
 //Fix scope issue/and functions
 function SubmitClick(){
-    let userAnswer = document.querySelector("#guessInput").value;
-    //DOM tree (then we get the element by ID), then value is attribute
-    invalidChecker(userAnswer);
-    //return true for invalid so string (works)|| constraints (dosen't)
-    triesLeft(userAnswer);
-    if (tries == 0){
-        playAgain = document.querySelector("#guessInput").value;
-        console.log(playAgain);
-        if(playAgain == "yes"){
-                GameStart();
-        } else{
-            console.log("Not work");
+    if(!WaitingGame()){ //wrong (fix later)
+        let userAnswer = document.querySelector("#guessInput").value;
+        //DOM tree (then we get the element by ID), then value is attribute
+        invalidChecker(userAnswer);
+        //return true for invalid so string (works)|| constraints (dosen't)
+        triesLeft(userAnswer);
+} else{
+        if (tries == 0){
+            playAgain = document.querySelector("#guessInput").value;
+            console.log(playAgain); //"" empty string
+            if(playAgain == "yes"){
+                    GameStart();
+            } else{
+                console.log("Not work");
+            }
         }
     }
- 
 
     }
 

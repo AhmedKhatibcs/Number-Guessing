8267db5aff6d4e3f771afac60c77794d3232fb22
AhmedKhatibcs <ahmedkhatibcs@gmail.com>
2026-09-07 18:51:55 -0700

    Reveal Button works and reveals the correct answer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 //global variable
 let tries = 10;
-GameStart();
+let randomNumberRound = 0;
+GameStart(); //(happens once in the loop)
+console.log("Game Start");
 
 function randomNumber(){
     return Math.floor(Math.random()*100);
@@ -12,39 +14,47 @@ function GameStart(){
         tries = 10;
     }
     document.getElementById("tries").innerText = ("Tries "+ 10);
-    randomNumber();
-    
+    randomNumberRound = randomNumber();
+    console.log(randomNumberRound);
+    return randomNumberRound;
 }
 
-
+function correctAnswer(userAnswer, randomNumber){
+    console.log(parseInt(userAnswer) === randomNumber);
+    if(parseInt(userAnswer) === randomNumber){
+            playAgain = document.querySelector("#guessInput").value;
+            if(playAgain == "yes"){
+                    GameStart();
+            } else{
+                console.log("Not work");
+            }
+        }
+    }
 //Function: Tries
 function triesLeft(userAnswer){
       if (tries == 1){
-            console.log("sorry out of luck!");
             tries -= 1;
             document.getElementById("tries").innerText = "sorry out of luck! Play Again, type \"yes";
             document.querySelector("#guessInput").value = "";
-            console.log(tries);
             //grabs "empty string"
     }
 
           else if(tries > 0) {
+            correctAnswer(userAnswer, randomNumberRound);
             tries -= 1;
             document.getElementById("tries").innerText = ("Tries "+ tries);
             console.log(userAnswer);
-            console.log(tries); 
     }
 }
 
 
-function validChecker(input){
+// function validChecker(input){
    
-}
+// }
 //Function: Invalid Checker
 function invalidChecker(userAnswer){
     //return true for invalid so string (works)|| constraints (dosen't)
     if (isNaN(userAnswer) || (0 > userAnswer  || userAnswer > 99)){
-        console.log("Sorry Invalid");
         document.querySelector("#guessInput").placeholder = "Invalid";
         // console.log(userAnswer);
         // Checking the box now in that moment
@@ -69,22 +79,17 @@ function SubmitClick(){
     if(!WaitingGame()){ //wrong (fix later)
         let userAnswer = document.querySelector("#guessInput").value;
         //DOM tree (then we get the element by ID), then value is attribute
-        invalidChecker(userAnswer);
-        //return true for invalid so string (works)|| constraints (dosen't)
+        invalidChecker(userAnswer); //This works
+    
         triesLeft(userAnswer);
-} else{
-        if (tries == 0){
-            playAgain = document.querySelector("#guessInput").value;
-            console.log(playAgain); //"" empty string
-            if(playAgain == "yes"){
-                    GameStart();
-            } else{
-                console.log("Not work");
-            }
-        }
-    }
+} 
 
     }
+
+function RevealClick(){
+    console.log(randomNumberRound);
+    document.getElementById("numbers").innerHTML = "The answer is: " + randomNumberRound;
+}
 
 
 
